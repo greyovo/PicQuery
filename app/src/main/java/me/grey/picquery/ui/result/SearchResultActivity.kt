f0159595de27ec9a315891ecfaca0f680edcbaac
Grey <grey030@foxmail.com>
2023-09-10 18:30:28 +0800

    pref: Polish result display page

--- app/src/main/java/me/grey/picquery/ui/result/SearchResultActivity.kt
+++ app/src/main/java/me/grey/picquery/ui/result/SearchResultActivity.kt
@@ -6,51 +6,12 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.FragmentActivity
-import me.grey.picquery.theme.PicQueryTheme
-import me.grey.picquery.themeM3.PicQueryThemeM3
 
 class SearchResultActivity : FragmentActivity() {
 
     private val viewModel: SearchResultViewModel by viewModels()
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val text = intent.getStringExtra("text")
@@ -58,65 +19,8 @@ class SearchResultActivity : FragmentActivity() {
             viewModel.startSearch(text)
         }
         setContent {
-            val searchText = remember { viewModel.searchText }
-            val state = rememberTopAppBarState()
-            val expand = remember { derivedStateOf { state.collapsedFraction >= 0.55f } }
-            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(state)
-
-            PicQueryThemeM3 {
-                Scaffold(
-                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-                    topBar = {
-                        LargeTopAppBar(
-                            title = {
-                                if (expand.value)
-                                    Text(text = "搜索: ${searchText.value}")
-                                else
-                                    Box(modifier = Modifier.padding(end = 12.dp)) { SearchBarInput() }
-                            },
-//                            title = { Text(text = "搜索: $searchText") },
-                            scrollBehavior = scrollBehavior,
-                            navigationIcon = {
-                                IconButton(onClick = { this.finish() }) {
-                                    Icon(
-                                        imageVector = Icons.Filled.ArrowBack,
-                                        contentDescription = "Return back"
-                                    )
-                                }
-                            }
-                        )
-                    },
-                ) { innerPadding ->
-                    SearchResultGrid(innerPadding)
-                }
-            }
+            SearchResultScreen(onBack = { this.finish() })
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    private fun SearchBarInput() {
-        val text = remember { viewModel.searchText }
-        fun action() {
-            // TODO onSearch
-            Log.d("onSearch", text.value)
-            viewModel.startSearch(text.value)
-        }
-        ProvideTextStyle(
-            value = MaterialTheme.typography.bodyLarge,
-        ) {
-            SearchBar(
-                modifier = Modifier.fillMaxWidth(),
-                query = text.value,
-                onQueryChange = { text.value = it },
-                onSearch = { action() },
-                active = false,
-                onActiveChange = { },
-                placeholder = { Text("对图片的描述...") },
-//            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-//            trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
-            ) {}
-        }
-
-    }
 }
--- app/src/main/java/me/grey/picquery/ui/result/SearchResultGrid.kt
+++ app/src/main/java/me/grey/picquery/ui/result/SearchResultGrid.kt
@@ -1,11 +1,15 @@
 package me.grey.picquery.ui.result
 
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,22 +18,70 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import me.grey.picquery.data.model.Photo
+import me.grey.picquery.themeM3.PicQueryThemeM3
 import java.io.File
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchResultScreen(
+    onBack: () -> Unit,
+    viewModel: SearchResultViewModel = viewModel()
+) {
+    val searchText = remember { viewModel.searchText }
+    val state = rememberTopAppBarState()
+    val expand = remember { derivedStateOf { false } }
+//            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(state)
+    PicQueryThemeM3 {
+        Scaffold(
+//                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "搜索结果") },
+//                            scrollBehavior = scrollBehavior,
+                    navigationIcon = {
+                        IconButton(onClick = { onBack() }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = "Return back"
+                            )
+                        }
+                    }
+                )
+            },
+        ) { innerPadding -> SearchResultGrid(innerPadding) }
+    }
+}
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalGlideComposeApi::class)
 @Composable
@@ -40,34 +92,79 @@ fun SearchResultGrid(
     val resultList by viewModel.resultList.collectAsState()
     val searching by viewModel.searchingState.collectAsState()
 
-    if (searching) {
-        CircularProgressIndicator()
-    } else if (resultList.isEmpty()) {
-        Text(text = "没有找到图片")
-    } else {
-        LazyVerticalGrid(
-            modifier = Modifier.padding(horizontal = 10.dp),
-            columns = GridCells.Adaptive(100.dp),
-            contentPadding = contentPadding,
-            content = {
-                val padding = Modifier.padding(3.dp)
-                item(span = { GridItemSpan(3) }) {
-                    Box(padding) {
-                        PhotoResultRecommend(photo = resultList[0], onItemClick = {})
-                    }
-                }
-                if (resultList.size > 1)
-                    items(
-                        resultList.size - 1,
-                        key = { resultList[it].id },
-                    ) { index ->
+    Column(
+        modifier = Modifier.padding(contentPadding)
+    ) {
+        SearchBarInput()
+        if (searching) {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                LinearProgressIndicator()
+            }
+        } else if (resultList.isEmpty()) {
+            Text(text = "没有找到图片")
+        } else {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(horizontal = 10.dp),
+                columns = GridCells.Adaptive(100.dp),
+                content = {
+                    val padding = Modifier.padding(3.dp)
+                    item(span = { GridItemSpan(3) }) {
                         Box(padding) {
-                            PhotoResultItem(resultList[index + 1], onItemClick = {})
+                            PhotoResultRecommend(photo = resultList[0], onItemClick = {})
                         }
                     }
-            }
-        )
+                    if (resultList.size > 1)
+                        items(
+                            resultList.size - 1,
+                            key = { resultList[it].id },
+                        ) { index ->
+                            Box(padding) {
+                                PhotoResultItem(resultList[index + 1], onItemClick = {})
+                            }
+                        }
+                }
+            )
+        }
     }
+
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchBarInput(
+    viewModel: SearchResultViewModel = viewModel()
+) {
+    val text = remember { viewModel.searchText }
+    fun action() {
+        // TODO onSearch
+        Log.d("onSearch", text.value)
+        viewModel.startSearch(text.value)
+    }
+    Box(
+        modifier = Modifier.padding(start = 14.dp, end = 14.dp, bottom = 14.dp)
+    ) {
+        val interactionSource = remember { MutableInteractionSource() }
+        DockedSearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = text.value,
+            onQueryChange = { text.value = it },
+            onSearch = { action() },
+            active = false,
+            onActiveChange = { },
+            placeholder = { Text("对图片的描述...") },
+            trailingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = null,
+                    Modifier.clickable { action() }
+                )
+            },
+//            trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+        ) {}
+    }
+
 }
 
 @ExperimentalFoundationApi
