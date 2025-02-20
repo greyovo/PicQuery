43e0800bb335e3907ec8ee269de5ef019775d2a9
CaptainZ <jelychow@gmail.com>
2025-02-20 19:47:14 +0800

    fix: fix build.gradle.kts was not configured right

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.google.oss.licenses.plugin)  {
+            exclude(group = "com.google.protobuf")
+        }
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+    alias(libs.plugins.ksp).apply(false)
+}
